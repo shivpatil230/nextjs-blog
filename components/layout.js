0d0e29e7b488983ba6e82b2155ed7163f3c21683
kaw393939 <kwilliam@njit.edu>
2023-03-28 06:13:55 -0400

    playwright testing

--- components/layout.js
+++ components/layout.js
@@ -7,7 +7,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = '[Your Name]'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'MyWebClass.org'
 
 export default function Layout({ children, home }) {
   return (
